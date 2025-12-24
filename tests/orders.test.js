0bc3df6e2ea3ac09079c35fbb4469963e68c7a22
Lucas Leung <kayauleung2024@gmail.com>
2025-12-24 22:35:24 +0800

    Create test for order api

--- tests/orders.test.js
+++ tests/orders.test.js
@@ -2,6 +2,11 @@ import { test, describe, expect, beforeAll } from "vitest";
 import cart from "../backend/api/cart.json";
 import { getTimeString } from "../data/orders.js";
 import { getDeliveryDate } from "../data/deliveryOption.js";
+import {
+  getMatchingProduct,
+  fetchProducts,
+  Products,
+} from "../data/products-backend.js";
 
 const response = await fetch("https://localhost:3001/orders", {
   method: "POST",
@@ -13,25 +18,55 @@ const response = await fetch("https://localhost:3001/orders", {
 const order = await response.json();
 console.log(order);
 
+await fetchProducts();
+
 describe("order api test", () => {
   test.concurrent("order id test", ({ expect }) => {
     expect(typeof order.id).toBe("string");
   });
 
   test.concurrent("order time test", ({ expect }) => {
-    expect(order.orderTime).not.toBe(null || undefined);
     expect(typeof order.orderTime).toBe("string");
+
     expect(getTimeString(order.orderTime)).toBe(getTimeString(new Date()));
   });
 
   test.concurrent("order products test", ({ expect }) => {
-    order.products.forEach((product, productNumber) => {
+    //test products length
+    expect(cart.length).toBe(order.products.length);
+
+    //test delivery time
+    order.products.forEach((products, productNumber) => {
+      expect(typeof products.estimatedDeliveryTime).toBe("string");
+
       const cartItem = cart[productNumber];
       const estimatedDeliveryTime = getDeliveryDate(cartItem.deliveryOptionId);
-      console.log(estimatedDeliveryTime);
       expect(estimatedDeliveryTime).toBe(
-        getTimeString(product.estimatedDeliveryTime)
+        getTimeString(products.estimatedDeliveryTime)
       );
     });
+
+    let matchingProduct;
+    cart.forEach((cartItem) => {
+      matchingProduct = order.products.find(
+        (product) => cartItem.productId === product.productId
+      );
+      //test product quantity
+      expect(matchingProduct.quantity).toBe(cartItem.quantity);
+    });
+
+    //test products id
+    expect(typeof matchingProduct).toBe("object");
+  });
+
+  test.concurrent("order cost test", ({ expect }) => {
+    expect(typeof order.totalCostCents).toBe("number");
+
+    let totalCostCents = 0;
+    cart.forEach((cartItem) => {
+      const product = getMatchingProduct(Products, cartItem.productId);
+      totalCostCents += product.priceCents;
+    });
+    expect(order.totalCostCents).toBe(totalCostCents);
   });
 });
