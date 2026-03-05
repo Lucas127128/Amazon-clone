79e47f2a7c2b0f6d469c7dd6f083d5698263e2a0
Lucas Leung <kayauleung2024@gmail.com>
2026-03-05 11:11:10 +0800

    Rename utils

---
+++ src/scripts/utils/trustedTypes.ts
@@ -0,0 +1,10 @@
+/// <reference lib="dom" />
+import DOMPurify from 'dompurify';
+
+export const policy = window.trustedTypes?.createPolicy('DOMPurify', {
+  createHTML: (inputHTML) =>
+    DOMPurify.sanitize(inputHTML, {
+      RETURN_TRUSTED_TYPE: false,
+      ADD_ATTR: ['fetchpriority', 'load', 'decode'],
+    }),
+});
