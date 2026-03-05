94a0d6c3df8433c0e265173b4d21905457a9e17d
Lucas Leung <kayauleung2024@gmail.com>
2026-03-05 12:43:30 +0800

    Create test for data/tracking.ts

---
+++ tests/normal/data/tracking.test.ts
@@ -0,0 +1,31 @@
+import {
+  describe,
+  expect,
+  test,
+  beforeAll,
+  afterAll,
+  setSystemTime,
+} from 'bun:test';
+import order from '../../order.json' with { type: 'json' };
+import cart from '../../cart.json' with { type: 'json' };
+import { getDeliveryProgress } from '#root/src/data/tracking.ts';
+import { Order } from '#root/src/data/orders.ts';
+import { Cart } from '#root/src/data/cart.ts';
+
+describe('test suite: getDeliveryProgress', () => {
+  beforeAll(() => {
+    const fakeTime = new Date('2026-03-05T12:00:00.000');
+    setSystemTime(fakeTime);
+  });
+  afterAll(() => {
+    setSystemTime();
+  });
+
+  test('get correct delivery progress', () => {
+    const deliveryProgress = getDeliveryProgress(
+      order as Order,
+      cart[0] as Cart,
+    );
+    expect(Math.round(deliveryProgress)).toBe(34);
+  });
+});
