f6b3b521807436b764c0858f70beba0fe09618cc
Lucas127128 <kayauleung2024@gmail.com>
2026-03-07 08:25:00 +0000

    Use knip to remove unused file and modules

--- server/src/api/convert.ts
+++
@@ -1,7 +0,0 @@
-import products from './rawProducts.json';
-import { nanoid } from 'nanoid';
-
-const newProducts = [...products];
-newProducts[42].id = nanoid(5);
-console.log(newProducts);
-await Bun.write('./src/api/rawProducts.json', JSON.stringify(newProducts));
